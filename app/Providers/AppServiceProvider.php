81cd9ec9aaebae796b33be9c853a1330353e9305
unknown <sebastesb1986@gmail.com>
2022-02-08 22:18:58 -0500

    add https file_2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+Illuminate\Routing\UrlGenerator
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https://');
+        }
     }
+
+    public function register()
+    {
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+    }
+
 }
